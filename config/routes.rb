2859cd54b93decfb712cde2fcde1be5856f401d2
Lauren <lemherdman@hotmail.com>
2015-11-12 17:03:41 -0500

    Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rainforest::Application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 end
