d3a895eb39082b23ec5b4dc85562e6e2482053b9
Lauren <lemherdman@hotmail.com>
2015-11-12 17:13:10 -0500

    Adding user names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to sessions_new_path, notice: "Signed up!"
+  		redirect_to '/sessions/new', notice: "Signed up!"
   	else
   		render "new"
   	end
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation)
+  	params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112212317) do
+ActiveRecord::Schema.define(version: 20151112220530) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20151112212317) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
