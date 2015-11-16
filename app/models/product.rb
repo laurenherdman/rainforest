4e320dc396ef7b7c0162b03e7eb2c11fe585c4ea
Lauren <lemherdman@hotmail.com>
2015-11-16 16:24:50 -0500

    Making it look pretty

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,8 @@ class Product < ActiveRecord::Base
 
 	def formatted_price
    price_in_dollars = price_in_cents.to_f / 100
-   sprintf("%.2f", price_in_dollars)
+   # sprintf("%.2f", price_in_dollars)
+   "$#{price_in_dollars}"
   end
 
   has_many :reviews
